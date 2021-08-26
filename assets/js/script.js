52a72ab5404bb4a04e8a57da30aaa2cddb5cfaa4
Cktestakis <codyktestakis@gmail.com>
2021-08-26 06:43:53 -0400

    Finished up the html and css code.

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//SECTION LIST
+const QUIZ_SECTION = document.querySelectorAll(".quiz-section")
+
+//START
+const START_SECTION = document.getElementById("start");
+const START_BTN = document.getElementById("start-button");
+
+//THE QUESTIONS
