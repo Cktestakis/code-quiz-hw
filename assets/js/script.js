4b465cb02d6561dbb167c9d8c4c3c48b28ee3560
Cktestakis <codyktestakis@gmail.com>
2021-08-27 07:01:36 -0400

    completed first script and new html for highscore

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,3 +62,233 @@ function startGame() {
 
     startTimer();
 }
+/*GAME START*/
+function startGame() {
+    showElement(QUIZ_SECTIONS, QUIZ_SECTION);
+
+    displayTime();
+    displayQuestion();
+
+    startTimer();
+}
+
+/*SHOW/HIDE ELEMENTS*/
+function showElement(siblingList, showElement) {
+    for (element of siblingList) {
+        hideElement(element);
+    }
+    showElement.classicList.remove("hidden");
+}
+
+function hideElement(element) {
+    if (!element.classicList.contains("hidden")) {
+        element.classicList.add("hidden");
+    }
+}
+
+/*TIME*/
+function displayTime() {
+    TIME_REMAINING.textContent = totalTime;
+}
+
+function startTimer() {
+    totalTimeInterval = setInterval(function() {
+        totalTime--;
+        displayTime();
+        checkTime();
+    }, 1000);
+}
+
+function checkTime() {
+    if (totalTime <= 0) {
+        totalTime = 0;
+        endGame();
+    }
+}
+
+/*QUESTIONS*/
+function displayQuestion() {
+    QUESTION.textContent = QUESTION_LIST[currentQuestion].question;
+
+    displayChoiceList();
+}
+
+function displayChoiceList() {
+    CHOICES.innterHTML = "";
+
+    QUESTION_LIST[currentQuestion].choices.forEach(function(answer, index) {
+        const li = document.createElement("li");
+        li.dataset.index = index;
+        const button = document.createElement("button");
+        button.textContent = (index + 1) + ". " + answer;
+        li.appendChild(button);
+        CHOICES.appendChild(li);
+    });
+}
+
+//When Questions are Answered//
+function processChoice(event) {
+    const userChoice = parseInt(event.target.parentElement.dataset.index);
+
+    resetChoiceStatusEffect();
+    checkChoice(userChoice);
+    getNextQuestion();
+}
+
+//Displaying Choice Status//
+function resetChoiceStatusEffect() {
+    clearTimeout(choiceStatusTimeout);
+    styleTimeRemainingDefault();
+}
+
+function styleTimeRemainingDefault() {
+    TIME_REMAINING.style.color = "#444";
+}
+
+function styleTimeRemainingWrong() {
+    TIME_REMAINING.style.color = "#E81648";
+}
+
+function checkChoice(userChoice) {
+    if (isChoiceCorrect(userChoice)) {
+        displayCorrectChoiceEffect();
+    } else {
+        displayWrongChoiceEffect();
+    }
+}
+
+function isChoiceCorrect(choice) {
+    return choice === QUESTION_LIST[currentQuestion].indexOfCorrectChoice;
+}
+
+function displayWrongChoiceEffect() {
+    deductTimeBy(10);
+
+    styleTimeRemainingWrong();
+    showElement(CHOICE_STATUS, WRONG);
+
+    choiceStatusTimeout = setTimeout(function() {
+        hideElement(WRONG);
+        styleTimeRemainingDefault;
+    }, 1000);
+}
+
+function deductTimeBy(seconds) {
+    totalTime -= seconds;
+    checkTime();
+    displayTime();
+}
+
+function displayCorrectChoiceEffect() {
+    showElement(CHOICE_STATUS, CORRECT);
+
+    choiceStatusTimeout = setTimeout(function() {
+        hideElement(CORRECT);
+    }, 1000);
+}
+
+//GET NEXT QUESTION//
+function getNextQuestion() {
+    currentQuestion++;
+    if (currentQuestion >= QUESTION_LIST.length) {
+        endGame();
+    } else {
+        displayQuestion();
+    }
+}
+
+/*END GAME*/
+function endGame() {
+    clearInterval(totalTimeInterval);
+
+    showElement(QUIZ_SECTIONS, END_SECTION);
+    displayScore();
+    setEndHeading();
+}
+
+function displayScore() {
+    SCORE.textContent = totalTime;
+}
+
+function setEndHeading() {
+    if (totalTime === 0) {
+        END_TITLE.textContent = "TIME IS OUT!";
+    } else {
+        END_TITLE.textContent = "CONGRATS! YOU FINISHED!";
+    }
+}
+
+/*SUBMITTING INITIALS*/
+function processInput(event) {
+    event.preventDefault();
+
+    const initials = INITIALS_INPUT.value.toUpperCase();
+
+    if (isInputValid(initials)) {
+        const score = totalTime;
+        const highscoreEntry = getNewHighscoreEntry(initials, score);
+        window.location.href="./highscores.html";
+    }
+}
+
+function getNewHighscoreEntry(initials, score) {
+    const entry = {
+        initials: initials,
+        score: score,
+    }
+    return entry;
+}
+
+function isInputValid(initials) {
+    let errorMessage = "";
+    if (initials === "") {
+        errorMessage = "You can't submit empty initials!";
+        displayFormError(errorMessage);
+        return false;
+    } else if (initials.match(/[^a-z]/ig)) {
+        errorMessage = "Initials may only include letters."
+        displayFormError(errorMessage);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function displayFormError(errorMessage) {
+    ERROR_MESSAGE.textContent  = errorMessage;
+    if (!INITIALS_INPUT.classList.contains("error")) {
+        INITIALS_INPUT.classList.add("error");
+    }
+}
+
+function saveHighscoreEntry(highscoreEntry) {
+    const currentScores = getScoreList();
+    placeEntryInHightscoreList(highscoreEntry, currentScores);
+    localStorage.setItem('scoreList', JSON.stringify(currentScores));
+}
+
+function getScoreList() {
+    const currentScores = localStorage.getItem ('scoreList');
+    if (currentScores) {
+        return JSON.parse(currentScores);
+    } else {
+        return [];
+    }
+}
+
+function placeEntryInHightscoreList(newEntry, scoreList) {
+    const newScoreIndex = getNewScoreIndex(newEntry, scoreList);
+    scoreList.splice(newScoreIndex, 0, newEntry);
+}
+
+function getNewScoreIndex(newEntry, scoreList) {
+    if (scoreList.length > 0) {
+        for (let i = 0; i < scoreList.length; i++) {
+            if (scoreList[i].score <= newEntry.score) {
+                return i;
+            }
+        }
+    }
+    return scoreList.length;
+}
+
