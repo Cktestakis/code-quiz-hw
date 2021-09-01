4510fe9003537a93907caa56f82c6389357869d3
Cody <codyktestakis@gmail.com>
2021-08-31 21:05:03 -0400

    Able to change every const to var

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,17 +31,18 @@ class Question {
         this.indexOfCorrectChoice = indexOfCorrectChoice;
     }
 }
-const QUESTION_1 = new Question("Which of the following is not a valid JavaScript variable name? ",
+
+var QUESTION_1 = new Question("Which of the following is not a valid JavaScript variable name? ",
 ["2names", "_first_and_last_names", "FirstAndLast", "None of the Above"], 1);
-const QUESTION_2 = new Question("____ tag is an extension to HTML that can enclose any number of JavaScript",
+var QUESTION_2 = new Question("____ tag is an extension to HTML that can enclose any number of JavaScript",
 ["<SCRIPT>", "<BODY>", "<HEAD>", "<TITLE>"], 1);
-const QUESTION_3 = new Question("The _________ method of an Array object adds and/or removes elements from an array", 
+var QUESTION_3 = new Question("The _________ method of an Array object adds and/or removes elements from an array", 
 ["Reverse", "Shift", "Slice", "Splice"], 3);
-const QUESTION_4 = new Question("Which of the following is not considered a JavaScript operator", 
+var QUESTION_4 = new Question("Which of the following is not considered a JavaScript operator", 
 ["new", "this", "delete", "typeof"], 1);
-const QUESTION_5 = new Question("In JavaScript, _______ is an object of the target language data type that encloses an object of the source language.", 
+var QUESTION_5 = new Question("In JavaScript, _______ is an object of the target language data type that encloses an object of the source language.", 
 ["a Wrapper", "a link", "a cursor", "a form"], 0);
-const QUESTION_LIST = [QUESTION_1, QUESTION_2, QUESTION_3, QUESTION_4, QUESTION_5];
+var QUESTION_LIST = [QUESTION_1, QUESTION_2, QUESTION_3, QUESTION_4, QUESTION_5];
 
 let currentQuestion = 0;
 
@@ -219,8 +220,8 @@ function processInput(event) {
     const initials = INITIALS_INPUT.value.toUpperCase();
 
     if (isInputValid(initials)) {
-        const score = totalTime;
-        const highscoreEntry = getNewHighscoreEntry(initials, score);
+        var score = totalTime;
+        var highscoreEntry = getNewHighscoreEntry(initials, score);
         window.location.href="./highscores.html";
     }
 }
